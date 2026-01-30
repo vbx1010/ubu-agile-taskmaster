6849c63596350686fd12fff9b8e427d6ea892a56
vbx1010 <bogdanbos00@gmail.com>
2026-01-30 12:50:59 +0100

    feat: estructura inicial del proyecto

---
+++ src/main/java/com/ubu/taskmaster/Main.java
@@ -0,0 +1,9 @@
+package com.ubu.taskmaster;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Iniciando TaskMaster v1.0...");
+        System.out.println("Sistema configurado correctamente con Maven.");
+        // TODO: Implementar lógica del menú aquí en el Sprint 1
+    }
+}
